deceb7787616c47611125448822804cf09e8d29f
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2023-04-01 14:03:43 +0200

    Initial commit - Create simple paint app

--- ptlab6/MainWindow.xaml.cs
+++ ptlab6/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ptlab6
@@ -20,9 +11,42 @@ namespace ptlab6
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point point = new Point();
+        private Brush brush = Brushes.Red;
+
         public MainWindow()
         {
             InitializeComponent();
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                brush = new SolidColorBrush(Color.FromRgb(colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B));
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line()
+                {
+                    X1 = point.X,
+                    Y1 = point.Y,
+                    X2 = e.GetPosition(this).X,
+                    Y2 = e.GetPosition(this).Y,
+                    Stroke = brush,
+                };
+                point = e.GetPosition(this);
+                canvas.Children.Add(line);
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                point = e.GetPosition(this);
+            }
         }
     }
 }
